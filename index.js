4f0e524d76a303e75d906d44db661cb2a852e1a0
luigi tanaka <luigi.tanaka12@gmail.com>
2022-06-23 17:53:54 -0300

    get participants

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 import express from "express";
 import cors from "cors";
 import chalk from "chalk";
+import dotenv from "dotenv";
+dotenv.config();
+
+import { MongoClient } from "mongodb";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+let db = null;
+const mongoClient = new MongoClient(process.env.URL_CONNECT_MONGO);
+const promise = mongoClient.connect();
+promise.then(() => {
+    db = mongoClient.db("UOL");
+});
+
+app.get("/participants", async (req, res) => {
+    try {
+        const participantes = await db.collection("participantes").find().toArray();
+        res.send(participantes);
+    } catch (error) {
+        res.status(500).send("Ocorreu algum problema ao tentar buscar os participantes");
+    }
+})
+
+
+
+
+app.listen(5000, () => {
+    console.log(chalk.bold.blue("Ta rodando!"));
+});
