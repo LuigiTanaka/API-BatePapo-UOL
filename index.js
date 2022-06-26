0931d647daf642a039cddc8a4bbc647c3e926c9a
luigi tanaka <luigi.tanaka12@gmail.com>
2022-06-26 13:06:15 -0300

    projeto final

--- index.js
+++ index.js
@@ -69,26 +69,34 @@ app.post("/messages", async (req, res) => {
     const { to, text, type } = req.body;
     const from = req.headers.user;
 
-    const messageSchema = joi.object({
-        to: joi.string().required(),
-        text: joi.string().required(),
-        type: joi.string().valid('message').valid('private_message'),
-        //falta verificar o from direitinho
-        from: joi.string()
-    });
+    try {
+        const participantes = await db.collection("participantes").find().toArray();
+        const nomeParticipantes = participantes.map(participante => participante.name);
 
-    const mensagem = {
-        to,
-        text,
-        type,
-        from
-    };
-
-    const validacao = messageSchema.validate(mensagem);
-    if (validacao.error) {
-        res.status(422).send("mensagem inválida");
-        return;
+        const messageSchema = joi.object({
+            to: joi.string().required(),
+            text: joi.string().required(),
+            type: joi.string().valid('message').valid('private_message'),
+            from: joi.string().valid(...nomeParticipantes)
+        });
+    
+        const mensagem = {
+            to,
+            text,
+            type,
+            from
+        };
+    
+        const validacao = messageSchema.validate(mensagem);
+        if (validacao.error) {
+            res.status(422).send("mensagem inválida");
+            return;
+        }
+    } catch (error) {
+        res.status(500).send("Ocorreu algum problema ao tentar validar a mensagem");
     }
+
+    
 
     try {
         await db.collection("mensagens").insertOne({
